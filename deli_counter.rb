059b00aeda9195be12d5db739a7eb21e54c662df
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 15:27:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli == [] ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli[0]}.")
+  katz_deli == [] ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli.shift}.")
 end
 
 
