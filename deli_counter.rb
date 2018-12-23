9f9fef581271e139a1b5de5de9a505dacd03b953
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 15:26:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
+def now_serving
+  katz_deli == [] ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli[0]}.")
+end
 
 
 #line(katz_deli)
