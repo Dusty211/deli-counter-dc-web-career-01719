b487dc41d236a3329c634c11bb25f928d55e23af
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 13:38:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+katz_deli = Array.new
+
+def line(katz_deli)
+  katz_deli == [] ? puts "The line is currently empty." : puts "The line is currently empty."
+end
+
+line(katz_deli)
+
