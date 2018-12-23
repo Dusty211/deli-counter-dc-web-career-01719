72ef077789eac54ae3d20167296ce64ef85e29c3
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 14:02:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ katz_deli = %w[Billy Jimmy Tom Mikey]
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
-  else katz_deli.each_with_index do |customer, index|
-    puts "The line is currently: #{index + 1}. #{customer}"
+  else 
+    puts "The line is currently: "
+    katz_deli.each_with_index do |customer, index|
+    puts "#{index + 1}. #{customer}"
   end
   end
 end
